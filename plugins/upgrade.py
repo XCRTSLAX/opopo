98f4cebe331cefdee63acc8df33139e5c6667d26
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-16 18:45:45 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -4,20 +4,6 @@ from pyrogram import Client , filters
 from helper.txt import kr
 
 
-@Client.on_callback_query(filters.regex('upgrade'))
-async def upgrade(bot,update):
-        await query.message.edit_text(
-	    text=kr.PAID_TXT,
-            reply_markup=InlineKeyboardMarkup([[ 
-                InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/Mr_Tamil_KiD") 
-                ],[
-                InlineKeyboardButton("PayPal 🌎",url = "https://t.me/Mr_Tamil_KiD"),
-                InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/1icxtwpo")
-                ],[
-                InlineKeyboardButton("Cancel",callback_data = "cancel")]])
-	
-	
-
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgrade(bot,message):
 	text = """**Free Plan User**
