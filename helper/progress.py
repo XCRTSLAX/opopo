c9970131433e4f8c2d5cc16d024ef56b6db37d8c
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-16 16:10:17 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -24,12 +24,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n**Progress**: {2}%\n".format(
+        progress = "{0}{1}".format(
             ''.join(["⦿" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⭗" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
-
-        tmp = progress + kr.PROGRESS_BAR.format(
+            ''.join(["⭗" for i in range(20 - math.floor(percentage / 5))]))
+            
+        tmp = progress + kr.PROGRESS_BAR.format( 
+            round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
@@ -38,7 +38,7 @@ async def progress_for_pyrogram(
         )
         try:
             await message.edit(
-                text="{}\n {}".format(
+                text="{}\n\n{}".format(
                     ud_type,
                     tmp
                 )
