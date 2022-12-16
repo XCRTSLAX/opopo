a4dcdfca33989c4eba714abfc1fc9db6d4734e8d
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-16 18:37:07 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -20,10 +20,29 @@ async def upgrade(bot,update):
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	await message.reply_text(
-            text=kr.PAID_TXT,
-            reply_markup=InlineKeyboardMarkup([[ 
-                InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/Mr_Tamil_KiD")], 
-        	[InlineKeyboardButton("PayPal 🌎",url = "https://t.me/Mr_Tamil_KiD"),
-        	InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/1icxtwpo")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	text = """**Free Plan User**
+	Daily  Upload limit 2GB
+	Price 0
 	
+	**VIP 1 ** 
+	Daily  Upload  limit 10GB
+	Price Rs 55  🇮🇳/🌎 0.67$  per Month
+	
+	**VIP 2 **
+	Daily Upload limit 50GB
+	Price Rs 80  🇮🇳/🌎 0.97$  per Month
+	
+	**VIP3**
+	Daily Upload limit 100GB
+	Price Rs 150  🇮🇳/🌎 1.81$  per Month
+	
+	
+	Pay Using Upi I'd ```tamildub@ybl```
+	
+	After Payment Send Screenshots Of 
+        Payment To Admin"""
+	keybord = InlineKeyboardMarkup([[ 
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/Mr_Tamil_KiD")], 
+        			[InlineKeyboardButton("PayPal 🌎",url = "https://t.me/Mr_Tamil_KiD"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/1icxtwpo")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
