6eb2bc1a05684d30e44601d2d70036c4b3d03e28
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-18 16:12:54 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -20,7 +20,7 @@ API_ID = int(os.environ.get("API_ID", "13384432"))
 
 API_HASH = os.environ.get("API_HASH", "ea9db4503ed7088b788e06dfd818e00e")
 
-STRING = os.environ.get("STRING", "AQAhvqqzDxOxM-GHPtjy2z1iZDJmqoi0H_R5V46S9D2nUT3kkfbQP1p89u9fnPK6T8jPvpJ7IMtfI4GM7D2OJjVI2WUjN3epcdrYuTf5wtsyhKUrioL9xigNzqzZV6T32jcAlgVbaBv_ioepOU-AlB9Omo-ulGR4yrXHBYHlx1OMNneYzH-8BLZO45-4oAApOAYBxvunvwSOn-bIyWchSv-XigLCOTFrsvd5frYpYQD00KUsk7T80boHOzPNI1a4jmK2vLyyZ-tb75xXeZDaYguQQV23QldgCxXsXz657XUkhslljkAJk7ar4cJsPNXs310ALXRD7Av0CE3xdV9K9bpKAAAAAUHleTIA")
+STRING = os.environ.get("STRING", "BQAWo3VJq8hDH8terPB7ckxOo3gKjTsx-CliB-A02IOIV3BJAWrAyXzpp6MJ43Lnl0vR7xIDsNnVLLOIjka8_nPWzCmuT1WGsU0c3xJMjZMhRPky8Cyty0OU6K1tZnZDF3jGViqvGasBX8OWblJv9pbRLmlvtEIiwvPENBLGSNN5VGTIC3QbIhvqXogtE_-aL-YgPOG-NX_YcTdrfq-gLVitYpkPjxeI6AnybT_zdaeRzaoovgkAtQ5r1PZFkumV-vwFcSH5DDVFopWSQpjXtQX_8TYnSMW0lvPTnwroC_ARRS5-QDudWYJOHWR4ugW0oSZFGGR6PrIHdr37YYtPOprvWbFknwA")
 
 app = Client("test", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 
