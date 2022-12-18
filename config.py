b598fdcf493a7c281b86219872caaf461544d70e
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-18 07:52:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", '48eb04ae416967495ba9930f87d4f4da')
     PAID_BOT = os.environ.get("PAID_BOT", "YES")
     STRING_SESSION = os.environ.get("STRING_SESSION", "BQBnrw8AFqfuh25HzE4bIsID4lnXwMZ3t16uPyXR_3Gu5QB45BLM2CHhoDiUJMpQ4Azl88qH5-9BGN45xP8Cx-zRXgIVez7JXPCo-PEkWCAaWovW7BTvozfEH6ZsniSIoMty_BbIQBEQj7oCWBZWEoKbWhGuQc5hzfb5HLXF4yUXKt-zW3XKCFacO5KoaUCCq1nRH2O4WmwlYsr967nag9nFxasm_zKm4uZbX0oR9lh0OYEcpWLaXka4KS_c9SxSRSVmwGuCbTwCuDl_6jzdCeuo-lwPMP62o8SvomgsysHa2EecrhfKQbA5Kpu1ISN8KAyhWXZKobopebqiOTojqrald20fBQAAAABuVPm6AA")
-    AUTH_USERS =  [int(i) for i in os.environ.get("AUTH_USERS", "1805398747").split(" ")]
+    AUTH_USERS =  [int(i) for i in os.environ.get("AUTH_USERS", "5400525106 1504797855").split(" ")]
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5028427286:AAGggDShZZwX7lnfkeaH9i0FalwBBkh2cGU")
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dkbotzon:dkbotzon@cluster0.q9dkemw.mongodb.net/?retryWrites=true&w=majority")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
