753bd81d79c3ae8c5d3859a852cc8e9252623189
KR-BOTZ <119985743+KR-BOTZ@users.noreply.github.com>
2022-12-21 00:04:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", '6795023'))
     API_HASH = os.environ.get("API_HASH", '48eb04ae416967495ba9930f87d4f4da')
     PAID_BOT = os.environ.get("PAID_BOT", "YES")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BQBnrw8AFqfuh25HzE4bIsID4lnXwMZ3t16uPyXR_3Gu5QB45BLM2CHhoDiUJMpQ4Azl88qH5-9BGN45xP8Cx-zRXgIVez7JXPCo-PEkWCAaWovW7BTvozfEH6ZsniSIoMty_BbIQBEQj7oCWBZWEoKbWhGuQc5hzfb5HLXF4yUXKt-zW3XKCFacO5KoaUCCq1nRH2O4WmwlYsr967nag9nFxasm_zKm4uZbX0oR9lh0OYEcpWLaXka4KS_c9SxSRSVmwGuCbTwCuDl_6jzdCeuo-lwPMP62o8SvomgsysHa2EecrhfKQbA5Kpu1ISN8KAyhWXZKobopebqiOTojqrald20fBQAAAABuVPm6AA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "BQDMOvAACtNs-VchfumyK9k5-nCNC07ktlBNprSCibT8v6TlKUld6ZCdmSweiwaB9dYKGUGUDJoVmwk76Di47l9KaeEfBO_6ly7autQqKVoaV0t51FOqxtQQ5sm1zKT4qfvV0zUnw-2nY4yUEzCJJ-2OADSkzQXUDWq_0R1WEDvDOrwUVu6Q92Yliu3yOvUe-yYF9Cd9aiUZO8oOl2Lr5ljVBOQXD1s-acZV_80Ltf20fTkuYym6Bv1zrdvyXuyiBBaQKv_cwHaXcjmAlxvmQaBE-N75q8C_TazXigo0Uflhpqj7MzZoLJZQWw15Tci7RITeErIKtRFC7u6JhnbLDIEZ_uuWYgAAAABZsWSfAA")
     AUTH_USERS =  [int(i) for i in os.environ.get("AUTH_USERS", "5400525106 1504797855").split(" ")]
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5028427286:AAGggDShZZwX7lnfkeaH9i0FalwBBkh2cGU")
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dkbotzon:dkbotzon@cluster0.q9dkemw.mongodb.net/?retryWrites=true&w=majority")
